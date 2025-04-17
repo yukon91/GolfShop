e2b79f8850c417d1031edb308938239e4c351402
ITSokje <oscar_kjellberg@hotmail.com>
2025-04-17 18:45:43 +0200

    Deleted Migrations

--- GolfShopHemsida/GolfShopHemsida/Migrations/20250417162725_InitialCreate.Designer.cs
+++ GolfShopHemsida/GolfShopHemsida/Migrations/20250417162725_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GolfShopHemsida.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20250417160710_InitialCreate")]
+    [Migration("20250417162725_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
